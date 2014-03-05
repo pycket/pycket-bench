5545a3272cd8e2c6a89f0d320c0ff0f248672372
Tobias Pape <tobias@netshed.de>
2014-03-05 19:05:08 +0100

    Opps, forgot the cooking step

--- setup.sh
+++ setup.sh
@@ -3,9 +3,12 @@
 PROGRAM=`echo $0 | sed 's%.*/%%'`
 PROGDIR="$(cd `dirname $0`; echo $PWD)"
 
-pushd $PROGDIR >/dev/null 2>/dev/null
+REBENCH="`command -v rebench`"
+if [ -z "$REBENCH" ]; then
+    pushd $PROGDIR >/dev/null 2>/dev/null
     pip install -r requirements.txt
-popd >/dev/null 2>/dev/null
+    popd >/dev/null 2>/dev/null
+fi
 
 RACKET="`command -v racket`"
 if [ -z "$RACKET" ]; then
@@ -18,4 +21,12 @@ if [ ! -f "$PROGDIR/pycket/targetpycket.py" ]; then
     git submodule init
     git submodule update
     popd >/dev/null 2>/dev/null
+fi
+
+if ls -1 "$PROGDIR/CrossBenchmarks/dish" | grep -q rkt; then
+    :
+else
+   pushd $PROGDIR/CrossBenchmarks/src  >/dev/null 2>/dev/null
+   sh cook_files.sh 
+   popd >/dev/null 2>/dev/null
 fi

dc1520e3ba884b4bef6f0a32f32e8354dd401d94
Tobias Pape <tobias@netshed.de>
2014-11-05 13:15:15 +0100

    Some fixes. Also: add v8/spidermonkey

--- setup.sh
+++ setup.sh
@@ -81,7 +81,7 @@ if [ ! -x "$PROGDIR/gambit/bin/gsc" ]; then
     _gone
 fi
 
-if  [ ! -x "$PROGDIR/larceny/larceny" ]; then
+if [ ! -x "$PROGDIR/larceny/larceny" ]; then
     $ECHO "installing Larceny"
     _go "$PROGDIR/src"
     if uname | grep -qi 'Darwin'; then
@@ -95,8 +95,70 @@ if  [ ! -x "$PROGDIR/larceny/larceny" ]; then
     _gone
 fi
 
-if [ ! -f "$PROGDIR/pycket/targetpycket.py" ]; then
-    $ECHO "fetching Pycket"
+
+if [ ! -x "$PROGDIR/v8/v8" ]; then
+    $ECHO "installing v8"
+    _go "$PROGDIR/src"
+    V8V=3.25.30
+    $FETCH https://github.com/v8/v8-git-mirror/archive/$V8V.tar.gz
+    tar -xzf "$V8V.tar.gz"
+    cd v8-git-mirror-$V8V
+    svn checkout \
+        http://gyp.googlecode.com/svn/trunk \
+        build/gyp --revision 1831
+    svn checkout --force --non-interactive --trust-server-cert \
+        https://src.chromium.org/chrome/trunk/deps/third_party/icu52 \
+        third_party/icu --revision 277999
+    if uname | grep -qi 'Darwin'; then
+        export CXX=clang++
+        export CC=clang
+        export CPP="clang -E"
+        export LINK=clang++
+        export CXX_host=clang++
+        export CC_host=clang
+        export CPP_host="clang -E"
+        export LINK_host=clang++
+        export GYP_DEFINES="clang=1"
+    fi
+    make x64.release -j4
+    ln -s shell out/x64.release/v8
+    ln -fs $PROGDIR/src/v8-git-mirror-$V8V/out/x64.release $PRODIR/v8
+    _gone
+fi
+
+if [ ! -x "$PRODIR/spidermonkery/bin/js24" ]; then
+    if uname | grep -qi 'Darwin' && type -fp brew >/dev/null; then
+      AUTOCONF=autoconf213
+      if [ ! \( -f /usr/local/bin/autoconf213 -a \
+                -f /usr/local/include/nspr/nspr.h \) ]; then
+          $ECHO "Please brew autoconf213 and nspr"
+          exit 1
+      fi
+    else
+      AUTOCONF=autoconf2.13
+      if [ ! \( -f /usr/bin/autoconf2.13 -a \
+                -f /usr/include/nspr/nspr.h \) ]; then
+          $ECHO "Please install nspr/libsnpr4-dev and autoconf2.13"
+          exit 1
+      fi
+    fi
+    _go "$PRODIR/src"
+    MJS=mozjs-24.2.0
+    $FETCH https://ftp.mozilla.org/pub/mozilla.org/js/$MJS.tar.bz2
+    tar -xjf $MFIL.tar.bz2
+    cd $MJS/js/src
+    $AUTOCONF
+    mkdir build-release
+    cd build-release
+    mkdir -p $PROGDIR/spidermonkey
+    ../configure --prefix=$PROGDIR/spidermonkey
+    make
+    make install
+    _gone
+fi
+
+if [ ! \( -f "pycket/targetpycket.py" -a -f "ChaperoneBenchmarks/README" \) ]; then
+    $ECHO "fetching deps (Pycket, Chaperone-benches)"
     _go $PROGDIR
     git submodule init
     git submodule update

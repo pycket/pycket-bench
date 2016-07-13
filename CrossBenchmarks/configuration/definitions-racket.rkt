2fbd41c60b7a7d30a223843c510a651cd94d8e58
Spenser Bauman <sabauma@gmail.com>
2016-07-13 12:04:32 -0400

    Add command line option for warmup iterations

--- CrossBenchmarks/configuration/definitions-racket.rkt
+++ CrossBenchmarks/configuration/definitions-racket.rkt
@@ -4,10 +4,20 @@
       (loop (+ i 1) (run))
       result)))
 
+(define warmup-iters
+ (let ([args (current-command-line-arguments)])
+   (if (< (vector-length args) 1) 0
+     (let ([n (string->number (vector-ref args 0))])
+       (if (fixnum? n) n
+         (error 'main "must have a fixnum argument"))))))
+
 (define (run-benchmark name count ok? run-maker . args)
   (newline)
   (let* ((run (apply run-maker args))
-         (result (time (run-bench name count ok? run))))
+         (result (begin
+                   (for ([_ (in-range warmup-iters)])
+                     (run-bench name count ok? run))
+                   (time (run-bench name count ok? run)))))
     (when (not (ok? result))
       (begin
         (display "*** wrong result ***")
